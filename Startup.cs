5e08ecdac9213af30b37744d6ff02db3b22d5eb2
David Stevens <james.stevens@healthcatalyst.com>
2021-06-11 09:05:59 -0600

    test commit

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,7 @@ namespace DotNetCoreSqlDb
         {
             services.AddControllersWithViews();
             services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlite("Data Source=localdatabase.db"));
+                    options.UseSqlServer(PublicSettings.ConnStr));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
